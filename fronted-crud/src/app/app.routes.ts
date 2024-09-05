1c5dcadebe7684739edfbeadca7cd100697e1aec
Dmarcela0 <dianagarzon0513@gmail.com>
2024-09-05 02:00:10 -0500

    configuracion fronted

--- fronted-crud/src/app/app.routes.ts
+++ fronted-crud/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegistroFormComponent } from './registro-form/registro-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        title: "Pagina de inicio"
+    },
+    {
+        path:'registro-form/:id',
+        component:RegistroFormComponent,
+        title: "Resgistro de usuarios"
+    },
+    {
+        path:'**',
+        redirectTo:'',
+        pathMatch:'full'
+    },
+];
