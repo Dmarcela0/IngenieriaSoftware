7baea1c3abed1cd2be0e8192ec560ff46caec2b5
Dmarcela0 <dianagarzon0513@gmail.com>
2024-09-05 19:16:48 -0500

    fronted
    Fronted

--- fronted-crud/src/app/home/home.component.ts
+++ fronted-crud/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { ButtonModule } from 'primeng/button';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   constructor() { }
